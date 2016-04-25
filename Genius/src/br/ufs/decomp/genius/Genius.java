0c8bd38a6fdd8e9c0b4b81dd6e47d0f36063d1a9
Afonso <Afonso@Fernando>
2016-04-24 23:11:22 -0300

    Definindo estrutura.

--- Genius/src/br/ufs/decomp/genius/Genius.java
+++ Genius/src/br/ufs/decomp/genius/Genius.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.ufs.yago.principal;
+package br.ufs.decomp.genius;
 
 /**
  *
---
+++ Genius/src/br/ufs/decomp/genius/regra/Controlador.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufs.decomp.genius.regra;
+
+/**
+ *
+ * @author Afonso
+ */
+public class Controlador {
+    
+}
